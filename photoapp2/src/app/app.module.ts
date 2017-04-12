fcfb6f30c0d63260485504f4e8b1119c1b666473
Priscyla Santos <santospriscyla@gmail.com>
2017-04-12 19:20:59 -0300

    Adicionando photoapp2

---
+++ photoapp2/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { PhotoApp2 } from './app.component';
+
+import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
+import { ItemDetailsPage } from '../pages/item-details/item-details';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    PhotoApp2 ,
+    HelloIonicPage,
+    ItemDetailsPage,
+    ListPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(PhotoApp2),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    PhotoApp2,
+    HelloIonicPage,
+    ItemDetailsPage,
+    ListPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
