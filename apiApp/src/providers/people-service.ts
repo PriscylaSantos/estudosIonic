022228e82c7e4795be9268038100c4e6db7bd913
Priscyla Santos <santospriscyla@gmail.com>
2017-04-16 02:36:40 -0300

    Buscando dados no arquivo api.json

--- apiApp/src/providers/people-service.ts
+++ apiApp/src/providers/people-service.ts
@@ -27,7 +27,8 @@ export class PeopleService {
     // We're using Angular HTTP provider to request the data,
     // then on the response, it'll map the JSON data to a parsed JS object.
     // Next, we process the data and resolve the promise with the new data.
-    this.http.get('https://randomuser.me/api/?results=10')
+    this.http.get('../assets/api.json')
+   // this.http.get('https://randomuser.me/api/?results=10')
       .map(res => res.json())
       .subscribe(data => {
         // we've got back the raw data, now generate the core schedule data
