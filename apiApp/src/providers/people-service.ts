12f83918999eaa51d808ac2904064da741b3d849
Priscyla Santos <santospriscyla@gmail.com>
2017-04-20 22:34:03 -0300

    Modificando apiApp

--- apiApp/src/providers/people-service.ts
+++ apiApp/src/providers/people-service.ts
@@ -27,7 +27,8 @@ export class PeopleService {
     // We're using Angular HTTP provider to request the data,
     // then on the response, it'll map the JSON data to a parsed JS object.
     // Next, we process the data and resolve the promise with the new data.
-    this.http.get('../assets/api.json')
+    //this.http.get('../assets/api.json')
+    this.http.get('../assets/teste.json')
    // this.http.get('https://randomuser.me/api/?results=10')
       .map(res => res.json())
       .subscribe(data => {
