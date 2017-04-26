4a6f6405f0c37056ef9696b45435898720b0c7fa
Priscyla Santos <santospriscyla@gmail.com>
2017-04-26 20:32:32 -0300

    Atualizando página de detalhes

--- githubIonic/src/pages/user-details/user-details.ts
+++ githubIonic/src/pages/user-details/user-details.ts
@@ -1,30 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { User } from '../../models/user';
-import { GithubUsers } from '../../providers/github-users';
-
-
+import { User } from "../../models/user";
+import { GithubUsers } from "../../providers/github-users";
 /**
  * Generated class for the UserDetails page.
  *
  * See http://ionicframework.com/docs/components/#navigation for more info
  * on Ionic pages and navigation.
  */
-//@IonicPage()
+
 @Component({
   selector: 'page-user-details',
   templateUrl: 'user-details.html',
 })
 export class UserDetailsPage {
   user: User;
-  login: string
+  login: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private githubUsers: GithubUsers) {
     this.login = navParams.get('login');
-    githubUsers.loadDetails(this.login).subscribe(user =>{
+    githubUsers.loadDetails(this.login).subscribe(user => {
       this.user = user;
-      console.log(user)
-    })
+      console.log(user);
+    });
   }
 
   ionViewDidLoad() {
